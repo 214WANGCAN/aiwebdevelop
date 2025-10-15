3c9186402f2444a8782ab3613bcc41ed03787038
silvervoid <wangcan20080128@qq.com>
2025-10-15 19:34:39 +0800

    我给改对了

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import routes from '~pages' // 👈 自动生成的路由表
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/GLF',
-      name: 'GLF',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/GLF.vue'),
-    },
-    {
-      path: 'TY',
-      name: 'TY',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/wyq.vue'),
-    },
-    {
-      path: '/wangcan',
-      name: 'wangcan',
-      component: () => import('../views/Wangcan.vue'),
-    },
-    {
-      path: '/tzl',
-      name: 'tzl',
-      component: () => import('../views/tzl.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
